9a4db606154a71ce5223cd93000c416ab5a96c93
Jo0oris <119428761+Jo0oris@users.noreply.github.com>
2026-03-31 11:33:55 +0200

    added regions to minimap_data.py

--- data/roi/minimap_data.py
+++ data/roi/minimap_data.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import cv2
 import numpy as np
 
+from util.bound_box import BoundingBox
 from util.screenlogger import logger
 
 # MINIMAP REGION
@@ -10,6 +11,55 @@ Y_END = 1040
 X_START = 800
 X_END = 1115
 
+regions = {
+    "corners": {
+        "left_top": BoundingBox(812, 864, 816, 868),
+        "left_bottom": BoundingBox(812, 1021, 816, 1025),
+        "right_bottom": BoundingBox(1100, 1021, 1104, 1025),
+        "right_top": BoundingBox(1100, 864, 1104, 868),
+    },
+
+    "sidelines": {
+        "top": BoundingBox(817, 864, 1099, 868),
+        "bottom": BoundingBox(817, 1021, 1099, 1025),
+    },
+
+    "baselines": {
+        "left": {
+            "goal_chance": BoundingBox(805, 910, 813, 978),
+            "top": BoundingBox(805, 864, 813, 909),
+            "bottom": BoundingBox(805, 979, 813, 1025),
+        },
+        "right": {
+            "goal_chance": BoundingBox(1082, 910, 1111, 978),
+            "top": BoundingBox(1082, 864, 1111, 909),
+            "bottom": BoundingBox(1082, 979, 1111, 1025),
+        }
+    },
+
+    "attacking_zones": {
+        "left": {
+            "top": BoundingBox(812, 868, 900, 899),
+            "middle": BoundingBox(812, 900, 900, 988),
+            "bottom": BoundingBox(812, 989, 900, 1020),
+        },
+        "left_mid": {
+            "top": BoundingBox(901, 868, 958, 899),
+            "middle": BoundingBox(901, 900, 958, 988),
+            "bottom": BoundingBox(901, 989, 958, 1020),
+        },
+        "right_mid": {
+            "top": BoundingBox(959, 868, 1016, 899),
+            "middle": BoundingBox(959, 900, 1016, 988),
+            "bottom": BoundingBox(959, 989, 1016, 1020),
+        },
+        "right": {
+            "top": BoundingBox(1017, 868, 1105, 899),
+            "middle": BoundingBox(1017, 900, 1105, 988),
+            "bottom": BoundingBox(1017, 989, 1105, 1020),
+        }
+    }
+}
 
 MINIMAP_LINES_MASKS = [
     # SIDE LINES
